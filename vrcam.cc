2ccfb8b24962868f3470107ed7fbaa1802c9e3a8
takumadx <hirayama.takuma@ohmydigifab.com>
2016-03-18 11:00:49 +0900

    debug

--- vrcam.cc
+++ vrcam.cc
@@ -1,4 +1,11 @@
 
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+
 int SaveJpegAsEquirectangular(int width, int height, int stride,
 		const uint8_t *imagedata, const char *out_filename) {
 	return 0;
