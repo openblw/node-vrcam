8b32e4109615adc0e6664dd1acfb3b53564f15e8
takumadx <hirayama.takuma@ohmydigifab.com>
2016-03-25 15:07:06 +0900

    bugfix

--- omxcv-config.h
+++ omxcv-config.h
@@ -7,11 +7,11 @@
 #define __OMXCV_CONFIG_H
 
 /* Short git hash */
-#define OMXCV_VERSION "${OMXCV_VERSION}"
+#define OMXCV_VERSION "73b931d"
 /* Date of git hash */
-#define OMXCV_DATE "${OMXCV_DATE}"
+#define OMXCV_DATE "2016-03-21 23:47:05 +0900"
 
 /* Enable Pi 2 (armv7/NEON) stuff */
-#cmakedefine ENABLE_NEON
+/* #undef ENABLE_NEON */
 
 #endif /* __OMXCV_CONFIG_H */
